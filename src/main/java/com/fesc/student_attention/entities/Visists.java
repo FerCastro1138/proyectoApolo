e6278bbfc2055d141c3955608fd518694b42596c
Fernando Castro <fercastro.live.com>
2025-02-09 15:54:20 -0600

    Creating visits class

---
+++ src/main/java/com/fesc/student_attention/entities/Visists.java
@@ -0,0 +1,30 @@
+package com.fesc.student_attention.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "visits")
+@Getter @Setter
+@ToString @EqualsAndHashCode
+@RequiredArgsConstructor
+public class Visists {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "folio", length = 8)
+    private String folio;
+    @Column(name = "next_visit")
+    private LocalDateTime nextVisit;
+    @Column(name = "hour")
+    private LocalTime hour;
+    @JoinColumn(name = "therapist_id", nullable = false)
+    private int therapisId;
+    @JoinColumn(name = "patient_id", nullable = false)
+    private int patientID;
+    @JoinColumn(name = "expedient_id", nullable = false)
+    private int expedientID;
+}
