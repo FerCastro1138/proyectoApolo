ba51d1e298a8bc9f0194af7f5b55a75272561563
Fernando Castro <fercastro.live.com>
2025-02-09 15:28:32 -0600

    Creating status entity with all anotations

--- src/main/java/com/fesc/student_attention/entities/Status.java
+++ src/main/java/com/fesc/student_attention/entities/Status.java
@@ -1,4 +1,19 @@
 package com.fesc.student_attention.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "status")
+@Getter @Setter
+@ToString @EqualsAndHashCode
+@RequiredArgsConstructor
 public class Status {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name", length = 10)
+    private String name;
+    @Column(name = "description", length = 50)
+    private String description;
 }
