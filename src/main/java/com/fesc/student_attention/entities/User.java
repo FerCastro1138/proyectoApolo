ee19eb9fec5e3d3fc0a39f898ff653df549cbb54
Fernando Castro <fercastro.live.com>
2025-02-09 15:29:23 -0600

    Creating user class with all anotations

--- src/main/java/com/fesc/student_attention/entities/User.java
+++ src/main/java/com/fesc/student_attention/entities/User.java
@@ -6,33 +6,36 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter @Setter
 @EqualsAndHashCode @ToString
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
-    @Column(name = "first_name")
+    @Column(name = "first_name", length = 29)
     private String firstName;
-    @Column(name = "middle_name")
+    @Column(name = "middle_name", length = 29)
     private String middleName;
-    @Column(name = "last_name")
+    @Column(name = "last_name", length = 29)
     private String lastName;
-    @Column(name = "second_last_name")
+    @Column(name = "second_last_name", length = 29)
     private String secondLastName;
-    @Column(name = "email")
+    @Column(name = "email", length = 254)
     private String email;
-    @Column(name = "password")
+    @Column(name = "password", length = 64)
     private char password;
-    @Column(name = "address")
+    @Column(name = "address", length = 100)
     private String address;
-    @Column(name = "gender")
+    @Column(name = "gender", length = 10)
     private String gender;
-    @Column(name = "date_of_birth")
-    private String dateOfBirth;
-    @Column(name = "number")
+    @Column(name = "date_of_birth", columnDefinition = "DATE")
+    private LocalDate dateOfBirth;
+    @Column(name = "number" , length = 10)
     private String number;
     @Column(name = "status_id")
     private int statusId;
