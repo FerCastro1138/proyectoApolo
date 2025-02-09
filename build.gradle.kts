bbe8025accc50c1fab937caa60f9f4bbf354aa0e
Fernando Castro <fercastro.live.com>
2025-02-09 16:20:48 -0600

    Adding hibernate types dependency from vladmihalcea

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.vladmihalcea:hibernate-types-60:2.21.1")
     // implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     // implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
