8f206a92e43ccc4146cfbe30867cd874cb29c514
Fernando Castro <fercastro.live.com>
2025-02-18 23:21:21 -0600

    Adding method to find role name

--- src/main/java/com/fesc/student_attention/repositories/RoleRepository.java
+++ src/main/java/com/fesc/student_attention/repositories/RoleRepository.java
@@ -4,7 +4,9 @@ import com.fesc.student_attention.entities.Role;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends CrudRepository<Role, Integer> {
-
+    Optional<Role> findRoleByName(String name);
 }
